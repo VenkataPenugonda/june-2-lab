6ee5f4f0547938c2f24bed118c267eae7862d865
VenkataPenugonda <65691075+VenkataPenugonda@users.noreply.github.com>
2021-06-23 23:35:03 -0400

    1

---
+++ IsItALeapYear/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace IsItALeapYear.Services
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Application to check if an year is leap year");
+        }
+        public bool IsItALeapYear(int year)
+        {
+            return year % 400 == 0 || (year % 4 == 0 && year % 100 != 0);
+        }
+    }
+}
